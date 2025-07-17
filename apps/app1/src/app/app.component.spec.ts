ac7fd78d1660d774220a0c16e11cc667c4aadb26
flywithmarth <118251187+phong2112@users.noreply.github.com>
2025-07-17 10:19:52 +0700

    Revert "test: make UT fail"

--- apps/app1/src/app/app.component.spec.ts
+++ apps/app1/src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('app1');
   });
 
   it('should render title', () => {
